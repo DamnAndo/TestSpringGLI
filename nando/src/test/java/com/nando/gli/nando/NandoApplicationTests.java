b45e40f9950621846d9529580d4b35e33d9a63cf
jeremiando <situmeang92.fs@gmail.com>
2020-07-14 10:42:40 +0700

    Template Spring

---
+++ nando/src/test/java/com/nando/gli/nando/NandoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nando.gli.nando;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NandoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
